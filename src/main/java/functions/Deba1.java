bf29cc20a3cbf979e74378df9cfe49ea069dcf0a
Vitalii Moholivskyi <mogolivskiy@gmail.com>
2019-04-09 09:51:57 +0300

    Fix Deba1

--- src/main/java/functions/Deba1.java
+++ src/main/java/functions/Deba1.java
@@ -8,7 +8,8 @@ public class Deba1 implements Function {
 		double res = 0;
 		
 		for(int i = 0; i < xVals.length; i++) {
-			res = res + Math.pow(Math.sin(5 * Math.PI + xVals[i]), 6);
+			System.out.println(xVals[i]);
+			res = res + Math.pow(Math.sin(5 * Math.PI * xVals[i]), 6);
 		}
 		
 		double temp = (double) 1 / xVals.length;
